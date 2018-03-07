759c5c09b3f4fd6515089e9b6b782eeb0779e8c0
Kyubinhan <kyubin@freshworks.io>
2018-03-07 13:06:11 -0800

    basic layout for manage zone component and view pdf file in Range use plan detail view

--- src/components/common/TextField.js
+++ src/components/common/TextField.js
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 export const TextField = ({ label, text, className = "", isLabelHidden = false }) => (
   <div className={classNames('text-field', className)}>
     <div 
-      className={classNames('text-field__label', {'field__label--hidden': isLabelHidden })}
+      className={classNames('text-field__label', {'text-field__label--hidden': isLabelHidden })}
     >
       {label}
     </div>
--- src/components/manageZone/ManageZone.js
+++ src/components/manageZone/ManageZone.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react';
+import { Banner } from '../common';
+import { Dropdown, Button } from 'semantic-ui-react';
 
 export class ManageZone extends Component {
   render() {
+    const zoneOptions = [];
+    const contactOptions = [];
+
     return (
-      <div>hello</div>
+      <div className="manage-zone">
+        <Banner 
+          header="Manage Zone"
+          content="This is where the description of Manage Zone would go. This is where the description of Manage Zone would go. 
+          This is where the description of Manage Zone would go."
+        />
+        <div className="manage-zone__content container">
+          <div className="manage-zone__steps">
+            <h3>Step 1: Pick A Zone</h3>
+            <div className="manage-zone__step-one">
+              <div className="manage-zone__dropdown">
+                <Dropdown placeholder='Zone' fluid search selection options={zoneOptions} />
+              </div>
+              <div className="manage-zone__text-field">Assigned Zone Contact</div>
+            </div>
+
+            <h3>Step 2: Change A Zone Contact</h3>
+            <div className="manage-zone__step-two">
+              <div className="manage-zone__text-field">Zone</div>
+              <div className="manage-zone__dropdown">
+                <Dropdown placeholder='Contact' fluid search selection options={contactOptions} />
+              </div>
+            </div>
+
+            <div className="manage-zone__update-btn">
+              <Button primary>Update Zone</Button>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
