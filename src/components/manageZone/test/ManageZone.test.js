586782fc675ea1db66fc9f32c3d3fc9cb5564bd9
Kyubinhan <kyubin@freshworks.io>
2018-03-14 10:00:36 -0700

    add unit tests for manage zone component

--- src/components/manageZone/test/ManageZone.test.js
+++ src/components/manageZone/test/ManageZone.test.js
@@ -16,4 +16,33 @@ describe('ManageZone', () => {
     const wrapper = shallow(<ManageZone {...props} />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  describe('Event handlers', () => {
+    it('`closeUpdateConfirmationModal` updates the state `isUpdateModalOpen` correctly', () => {
+      const wrapper = shallow(<ManageZone {...props} />);
+      wrapper.instance().closeUpdateConfirmationModal();
+      expect(wrapper.state().isUpdateModalOpen).toEqual(false);
+
+      wrapper.instance().openUpdateConfirmationModal();
+      expect(wrapper.state().isUpdateModalOpen).toEqual(true);
+    });
+
+    it('`onContactChanged` updates the state `newContact` correctly', () => {
+      const wrapper = shallow(<ManageZone {...props} />);
+      const mockValue = 'mockValue';
+      wrapper.instance().onContactChanged({}, { value: mockValue });
+
+      expect(wrapper.state().newContact).toEqual(mockValue);      
+    });
+
+    it('`onZoneChanged` updates the state `zone` and `currContact` correctly', () => {
+      const wrapper = shallow(<ManageZone {...props} />);
+      const mockValue = 'mockValue';
+      expect(wrapper.state().currContact).toEqual(null);    
+      
+      wrapper.instance().onZoneChanged({}, { value: mockValue });
+      expect(wrapper.state().zone).toEqual(mockValue);
+      expect(wrapper.state().currContact).not.toEqual(null);    
+    });
+  });
 });
