d9003ed3993440389095f4dd02ea6dc3f826947a
Kyubinhan <kyubin@freshworks.io>
2018-04-17 09:31:38 -0700

    base url change

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,14 +3,20 @@ export const SSO_REALM_NAME = 'mobile';
 export const SSO_BASE_AUTH_ENDPOINT = `${SSO_BASE_URL}auth/realms/mobile/protocol/openid-connect/auth`;
 export const SSO_CLIENT_ID = 'range-test';
 export const SSO_REDIRECT_URI = `${window.location.origin}/login`;
+// export const SSO_REDIRECT_URI = 'https://web-range-myra-dev.pathfinder.gov.bc.ca'
 
 export const SSO_AUTH_ENDPOINT = `${SSO_BASE_AUTH_ENDPOINT}?response_type=code&client_id=${SSO_CLIENT_ID}&redirect_uri=${SSO_REDIRECT_URI}`;
 
 export const GET_TOKEN = `auth/realms/${SSO_REALM_NAME}/protocol/openid-connect/token`;
 export const REFRESH_TOKEN = `auth/realms/${SSO_REALM_NAME}/protocol/openid-connect/token`;
 
-export const BASE_URL = 'http://api-range-myra-dev.pathfinder.gov.bc.ca/v1';
-// export const BASE_URL = 'http://localhost:8000/v1';
+// For production
+export const BASE_URL = (process.env.NODE_ENV === 'production') ? 'api/v1' : 'http://api-range-myra-dev.pathfinder.gov.bc.ca/api/v1';
+
+// For development
+// export const BASE_URL = 'http://web-range-myra-dev.pathfinder.gov.bc.ca/api/v1';
+// export const BASE_URL = 'http://localhost:8000/api/v1';
+
 export const AGREEMENT = '/agreement';
 export const REFERENCES = '/reference';
 export const CLIENT = '/client';
