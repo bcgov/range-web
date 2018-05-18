21f66242a909592e0391fbbc32e4919b8e471536
Kyubinhan <kyubin@freshworks.io>
2018-05-18 11:40:44 -0700

    reorganize and refactor handler functions

--- src/handlers/index.js
+++ src/handlers/index.js
@@ -1,5 +1,4 @@
 import moment from 'moment';
-import queryParams from 'query-params';
 import {
   NOT_PROVIDED,
   NP,
@@ -210,26 +209,6 @@ export const getErrorMessage = (err) => {
 };
 
 /**
- * Turn a string parameter into an object with the key and value pair
- *
- * @param {string} query The stringified query parameter
- * @returns A plain object
- */
-export const parseQuery = (query = '') => (
-  queryParams.decode(query.replace(/\?/g, ''))
-);
-
-/**
- * Turn a query object into a web link parameter looking string
- *
- * @param {object} query The plain object with the key and value pair
- * @returns A stringified query parameter
- */
-export const stringifyQuery = query => (
-  queryParams.encode(query)
-);
-
-/**
  * Download a pdf file through an a tag using a built-in browser feature
  *
  * @param {response.data} blob The binary array buffer from API
@@ -272,8 +251,8 @@ export const downloadPDFBlob = (blob, ref, fileName) => {
 
 const round = (number, precision) => {
   const shift = (number, precision) => {
-    const numArray = ('' + number).split('e');
-    return +(numArray[0] + 'e' + (numArray[1] ? (+numArray[1] + precision) : precision));
+    const numArray = (`${number}`).split('e');
+    return +(`${numArray[0]}e${(numArray[1] ? (+numArray[1] + precision) : precision)}`);
   };
   return shift(Math.round(shift(number, +precision)), -precision);
 };
@@ -286,4 +265,53 @@ const round = (number, precision) => {
  */
 export const roundTo1Decimal = number => (
   round(number, 1)
+);
+
+const encode = (o, sep) => {
+  const list = [];
+  Object.keys(o).map((key) => {
+    if (o[key] != null && typeof o[key] !== 'object' && typeof o[key] !== 'function') {
+      list.push(`${encodeURIComponent(key)}=${encodeURIComponent(o[key])}`);
+    }
+  });
+  return list.join(sep || '&');
+};
+
+const REXP_SPLIT = /&amp;|&|;/gmi;
+const decode = (str, s) => {
+  const sep = s || REXP_SPLIT;
+  const result = {};
+  const expr = str.split(sep);
+  let key;
+  let val;
+  let index;
+  for (let i = 0, len = expr.length; i < len; i += 1) {
+      index = expr[i].indexOf('=');
+      key = expr[i].substring(0, index);
+      val = expr[i].substring(index + 1);
+      if (val) {
+          result[decodeURIComponent(key)] = decodeURIComponent(val);
+      }
+  }
+  return result;
+};
+
+/**
+ * Turn a string parameter into an object with the key and value pair
+ *
+ * @param {string} query The stringified query parameter
+ * @returns A plain object
+ */
+export const parseQuery = (query = '') => (
+  decode(query.replace(/\?/g, ''))
+);
+
+/**
+ * Turn a query object into a web link parameter looking string
+ *
+ * @param {object} query The plain object with the key and value pair
+ * @returns A stringified query parameter
+ */
+export const stringifyQuery = query => (
+  encode(query)
 );
