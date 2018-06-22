0697a2391460695d25adc4399d7b508464673fa4
Kyubinhan <kyubin@freshworks.io>
2018-06-21 17:21:07 -0700

    add constants

---
+++ src/constants/variables.js
@@ -0,0 +1,59 @@
+export const LOCAL_STORAGE_KEY = {
+  AUTH: 'range-web-auth',
+  USER: 'range-web-user',
+  REFERENCE: 'range-web-reference',
+};
+
+export const PLAN_STATUS = {
+  PENDING: 'Pending',
+  COMPLETED: 'Completed',
+  DRAFT: 'Draft',
+  CREATED: 'Created',
+  CHANGE_REQUESTED: 'Change Requested',
+};
+
+export const REFERENCE_KEY = {
+  PLAN_STATUS: 'PLAN_STATUS',
+  LIVESTOCK_TYPE: 'LIVESTOCK_TYPE',
+  LIVESTOCK_IDENTIFIER_TYPE: 'LIVESTOCK_IDENTIFIER_TYPE',
+  MINISTER_ISSUE_TYPE: 'MINISTER_ISSUE_TYPE',
+  MINISTER_ISSUE_ACTION_TYPE: 'MINISTER_ISSUE_ACTION_TYPE',
+  CLIENT_TYPE: 'CLIENT_TYPE',
+  AGREEMENT_EXEMPTION_STATUS: 'AGREEMENT_EXEMPTION_STATUS',
+};
+
+export const IMAGE_SRC = {
+  NAV_LOGO: '/logo.png',
+  COW_PIC: '/cow.jpg',
+  LOGIN_LOGO: '/login_logo.png',
+};
+
+export const CLIENT_TYPE = {
+  PRIMARY: 'A',
+  OTHER: 'B',
+};
+
+export const DATE_FORMAT = {
+  SCHEUDLE_ENTRY: 'MMM D',
+  SERVER_SIDE: 'YYYY-MM-DDTHH:mm:ss.SSSZ',
+  CLIENT_SIDE: 'MMMM D, YYYY',
+};
+
+export const ROLE = {
+  ADMINISTRATOR: 'myra_admin',
+  RANGE_OFFICER: 'myra_range_officer',
+  AGREEMENT_HOLDER: 'myra_client',
+};
+
+export const ELEMENT_ID = {
+  GRAZING_SCHEDULE_ELEMENT_ID: 'rup__grazing-schedule',
+  RUP_ZONE_DROPDOWN_ELEMENT_ID: 'rup__zone-dropdown',
+  RUP_STICKY_HEADER_ELEMENT_ID: 'rup-sticky-header',
+  SIGN_OUT_ELEMENT_ID: 'sign-out',
+  MANAGE_ZONE_ZONES_DROPDOWN_ELEMENT_ID: 'manage-zone__zone-dropdown',
+  MANAGE_ZONE_CONTACTS_DROPDOWN_ELEMENT_ID: 'manage-zone__contact-dropdown',
+  LOGIN_BUTTON_ELEMENT_ID: 'login-button',
+  SEARCH_TERM: 'searchTerm',
+  MANAGE_CLIENT_USERS_DROPDOWN_ELEMENT_ID: 'manage-client__users-dropdown',
+  MANAGE_CLIENT_CLIENTS_DROPDOWN_ELEMENT_ID: 'manage-client__clients-dropdown',
+};
