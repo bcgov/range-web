0187730b96a1f9abc42cc01a263a966bd3f21526
Kyubinhan <kyubin@freshworks.io>
2018-09-04 15:29:14 -0700

    eject create-react-app with more configuration

---
+++ scripts/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const fs = require('fs');
+const paths = require('../config/paths');
+
+const app = express();
+const port = 3000;
+
+const staticServe = express.static(paths.appBuild, {
+  immutable: true,
+  maxAge: '1y',
+});
+
+const serveGzipped = (contentType) => (req, res, next) => {
+  const acceptedEncodings = req.acceptsEncodings();
+  if (acceptedEncodings.indexOf('gzip') === -1 || !fs.existsSync(`./build/${req.url}.gz`)) {
+    next();
+    return;
+  }
+
+  // update request's url
+  req.url = `${req.url}.gz`;
+
+  // set correct headers
+  res.set('Content-Encoding', 'gzip');
+  res.set('Content-Type', contentType);
+
+  // let express.static take care of the updated request
+  next();
+}
+
+
+app.get('*.js', serveGzipped('text/javascript'));
+app.get('*.css', serveGzipped('text/css'));
+
+app.get('/service-worker.js', (req, res) => {
+  res.set({ 'Content-Type': 'application/javascript; charset=utf-8' });
+  res.send(fs.readFileSync('./build/service-worker.js'));
+});
+
+app.use('/', staticServe);
+app.use('*', staticServe);
+
+app.listen(port, () => console.log(`Server running in port ${port}`));
