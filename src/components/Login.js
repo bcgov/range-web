eb8d5b9f31a7e27753c0b03f20fa26db9a480751
Kyubinhan <kyubin@freshworks.io>
2018-09-11 14:26:23 -0700

    include Edge to be avaliable in warning section

--- src/components/Login.js
+++ src/components/Login.js
@@ -60,6 +60,7 @@ export class Login extends Component {
                 Please visit {APP_NAME} using a supported browser:
                 <a href="https://www.google.com/chrome" target="_blank" rel="noopener noreferrer"> Google Chrome,</a>
                 <a href="https://www.mozilla.org/en-US/firefox/new" target="_blank" rel="noopener noreferrer"> Firefox,</a>
+                <a href="https://www.microsoft.com/en-ca/windows/microsoft-edge" target="_blank" rel="noopener noreferrer"> Microsoft Edge,</a>
                 <a href="https://support.apple.com/en-ca/safari" target="_blank" rel="noopener noreferrer"> Safari </a>
                 (Mac only).
               </div>
