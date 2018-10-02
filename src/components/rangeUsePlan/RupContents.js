36d077e2475ef53417e44470b2e8f19d23afa1ff
Kyubinhan <kyubin@freshworks.io>
2018-10-02 14:06:47 -0700

    indicate the current tab in detail rup view

--- src/components/rangeUsePlan/RupContents.js
+++ src/components/rangeUsePlan/RupContents.js
@@ -1,11 +1,14 @@
+/* eslint-disable */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import classnames from 'classnames';
 import { ELEMENT_ID, IMAGE_SRC } from '../../constants/variables';
 import { MINISTER_ISSUES, SCHEDULES, PASTURES, BASIC_INFORMATION } from '../../constants/strings';
-
 class RupContents extends Component {
   static propTypes = {
     children: PropTypes.node.isRequired,
+    location: PropTypes.shape({}).isRequired,
   };
 
   renderChild = (child, index) => {
@@ -23,24 +26,37 @@ class RupContents extends Component {
   }
 
   render() {
-    const { children } = this.props;
+    const { children, location, history } = this.props;
+    const { hash } = location;
 
     return (
       <div className="rup__contents__container">
         <div className="rup__contents__tabs">
-          <a href={`#${ELEMENT_ID.BASIC_INFORMATION}`}>
+          <a
+            href={`#${ELEMENT_ID.BASIC_INFORMATION}`}
+            className={classnames('rup__contents__tab', { 'rup__contents__tab--active': hash === `#${ELEMENT_ID.BASIC_INFORMATION}` })}
+          >
             <img src={IMAGE_SRC.BASIC_INFORMATION_ICON} alt="icon" />
             <span>{BASIC_INFORMATION}</span>
           </a>
-          <a href={`#${ELEMENT_ID.PASTURES}`}>
+          <a
+            href={`#${ELEMENT_ID.PASTURES}`}
+            className={classnames('rup__contents__tab', { 'rup__contents__tab--active': hash === `#${ELEMENT_ID.PASTURES}` })}
+          >
             <img src={IMAGE_SRC.PASTURES_ICON} alt="icon" />
             <span>{PASTURES}</span>
           </a>
-          <a href={`#${ELEMENT_ID.GRAZING_SCHEDULE}`}>
+          <a
+            href={`#${ELEMENT_ID.GRAZING_SCHEDULE}`}
+            className={classnames('rup__contents__tab', { 'rup__contents__tab--active': hash === `#${ELEMENT_ID.GRAZING_SCHEDULE}` })}
+          >
             <img src={IMAGE_SRC.SCHEDULES_ICON} alt="icon" />
             <span>{SCHEDULES}</span>
           </a>
-          <a href={`#${ELEMENT_ID.MINISTER_ISSUES}`}>
+          <a
+            href={`#${ELEMENT_ID.MINISTER_ISSUES}`}
+            className={classnames('rup__contents__tab', { 'rup__contents__tab--active': hash === `#${ELEMENT_ID.MINISTER_ISSUES}` })}
+          >
             <img src={IMAGE_SRC.MINISTER_ISSUES_ICON} alt="icon" />
             <span>{MINISTER_ISSUES}</span>
           </a>
@@ -53,4 +69,4 @@ class RupContents extends Component {
   }
 }
 
-export default RupContents;
+export default withRouter(RupContents);
