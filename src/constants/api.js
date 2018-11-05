b356b72abe9c8751f887069bc04ebc6200815f59
Kyubinhan <kyubin@freshworks.io>
2018-11-05 11:41:10 -0800

    test for sso base url auto filled by caddy

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,10 @@
 // export const SSO_REALM_NAME = 'mobile';
 // export const SSO_CLIENT_ID = 'range-test';
 
-export const SSO_BASE_URL = 'https://sso-dev.pathfinder.gov.bc.ca';
+export const SSO_BASE_URL = (process.env.NODE_ENV === 'production')
+  ? '{{.Env.SSO_BASE_URL}}' // caddy will put this when serving the content
+  : 'https://sso-dev.pathfinder.gov.bc.ca';
+
 export const SSO_REALM_NAME = 'range';
 export const SSO_CLIENT_ID = 'myrangebc';
 export const SSO_BASE_AUTH_ENDPOINT = `${SSO_BASE_URL}/auth/realms/${SSO_REALM_NAME}/protocol/openid-connect`;
