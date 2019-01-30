e16b73b6840a3363d6b5c20c040bdc7d396cf16c
Kyubinhan <kyubin@freshworks.io>
2019-01-30 15:45:58 -0800

    create helper functions to handle keypress

---
+++ src/utils/helper/input.js
@@ -0,0 +1,15 @@
+export const allowNumberOnly = (e) => {
+  if (!(e.charCode >= 48 && e.charCode <= 57)) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+};
+
+export const allowAlphabetOnly = (e) => {
+  const regex = new RegExp('^[a-zA-Z ]+$');
+  const key = String.fromCharCode(!e.charCode ? e.which : e.charCode);
+  if (!regex.test(key)) {
+    e.preventDefault();
+    e.stopPropagation();
+  }
+};
