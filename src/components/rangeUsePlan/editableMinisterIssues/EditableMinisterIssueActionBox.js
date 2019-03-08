3edf12d1478f3082aa861e1ab3dc050c5d874f5e
Kyubinhan <kyubin@freshworks.io>
2019-03-07 16:09:55 -0800

    include missing No Graze Period fields for minister issue action

--- src/components/rangeUsePlan/editableMinisterIssues/EditableMinisterIssueActionBox.js
+++ src/components/rangeUsePlan/editableMinisterIssues/EditableMinisterIssueActionBox.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Pikaday from 'pikaday';
+import classnames from 'classnames';
 import { Input, Icon, TextArea, Dropdown, Form } from 'semantic-ui-react';
-import { REFERENCE_KEY, CONFIRMATION_MODAL_ID } from '../../../constants/variables';
+import { REFERENCE_KEY, CONFIRMATION_MODAL_ID, DATE_FORMAT } from '../../../constants/variables';
 import { DELETE_MINISTER_ISSUE_ACTION_CONFIRM_CONTENT, DELETE_MINISTER_ISSUE_ACTION_CONFIRM_HEADER } from '../../../constants/strings';
+import { parseMonthAndDay } from '../../../utils';
 
 class EditableMinisterIssueActionBox extends Component {
   static propTypes = {
@@ -14,6 +17,68 @@ class EditableMinisterIssueActionBox extends Component {
     handleActionChange: PropTypes.func.isRequired,
     handleActionDelete: PropTypes.func.isRequired,
   };
+
+  componentDidMount() {
+    const { action } = this.props;
+    const {
+      noGrazeEndDay: ngEndDay,
+      noGrazeEndMonth: ngEndMonth,
+      noGrazeStartDay: ngStartDay,
+      noGrazeStartMonth: ngStartMonth,
+    } = action;
+
+    // use the current year as a reference point to create date objects
+    const currYear = new Date().getFullYear();
+    const noGrazeStartDate = (ngStartDay && ngStartMonth) ? new Date(`${currYear}-${ngStartMonth}-${ngStartDay}`) : undefined;
+    const noGrazeEndDate = (ngEndDay && ngEndMonth) ? new Date(`${currYear}-${ngEndMonth}-${ngEndDay}`) : undefined;
+
+    const minDate = new Date(`${currYear}-01-02`);
+    const maxDate = new Date(`${currYear + 1}-01-01`);
+
+    this.pikaDayDateIn = new Pikaday({
+      field: this.startDateRef,
+      format: DATE_FORMAT.CLIENT_SIDE_WITHOUT_YEAR,
+      minDate,
+      maxDate: noGrazeEndDate || maxDate,
+      defaultDate: noGrazeStartDate || minDate, // the initial date to view when first opened
+      setDefaultDate: noGrazeStartDate !== undefined,
+      onSelect: this.handleDateChange('noGrazeStartDate'),
+    });
+
+    this.pikaDayDateOut = new Pikaday({
+      field: this.endDateRef,
+      format: DATE_FORMAT.CLIENT_SIDE_WITHOUT_YEAR,
+      minDate: noGrazeStartDate || minDate,
+      maxDate,
+      defaultDate: noGrazeEndDate || minDate,
+      setDefaultDate: noGrazeEndDate !== undefined,
+      onSelect: this.handleDateChange('noGrazeEndDate'),
+    });
+  }
+
+  handleDateChange = key => (date) => {
+    const { action, actionIndex, handleActionChange } = this.props;
+    const newAction = {
+      ...action,
+    };
+
+    if (this.pikaDayDateIn && key === 'noGrazeEndDate') {
+      // needs to save the month and day separately as integers
+      const { month: noGrazeEndMonth, day: noGrazeEndDay } = parseMonthAndDay(date);
+      newAction.noGrazeEndMonth = noGrazeEndMonth;
+      newAction.noGrazeEndDay = noGrazeEndDay;
+
+      this.pikaDayDateIn.setMaxDate(date);
+    } else if (this.pikaDayDateOut && key === 'noGrazeStartDate') {
+      const { month: noGrazeStartMonth, day: noGrazeStartDay } = parseMonthAndDay(date);
+      newAction.noGrazeStartMonth = noGrazeStartMonth;
+      newAction.noGrazeStartDay = noGrazeStartDay;
+
+      this.pikaDayDateOut.setMinDate(date);
+    }
+
+    handleActionChange(newAction, actionIndex);
+  }
 
   onActionFieldChanged = (e, { name, value }) => {
     const {
@@ -54,6 +119,9 @@ class EditableMinisterIssueActionBox extends Component {
     });
   }
 
+  setStartDateRef = (ref) => { this.startDateRef = ref; }
+  setEndDateRef = (ref) => { this.endDateRef = ref; }
+
   render() {
     const { action, references } = this.props;
     const { detail, actionTypeId, other } = action;
@@ -73,8 +141,10 @@ class EditableMinisterIssueActionBox extends Component {
     const ellipsisOptions = [
       { key: 'delete', text: 'Delete', onClick: this.openDeleteActionConfirmationModal },
     ];
-    const otherActionType = actionTypes.find(t => t.name === 'Other');
+    const otherActionType = actionTypes.find(type => type.name === 'Other');
+    const timingActionType = actionTypes.find(type => type.name === 'Timing');
     const isActionTypeOther = otherActionType && (actionTypeId === otherActionType.id);
+    const isActionTypeTiming = timingActionType && (actionTypeId === timingActionType.id);
 
     return (
       <div className="rup__missue__action">
@@ -96,9 +166,30 @@ class EditableMinisterIssueActionBox extends Component {
                 value={other || ''}
                 onChange={this.onActionFieldChanged}
                 style={{ marginLeft: '5px' }}
-                // label="Name"
               />
             }
+            <div
+              className={classnames(
+                'rup__missue__action__ng',
+                { 'rup__missue__action__ng--hidden': !isActionTypeTiming },
+              )}
+            >
+              No Graze Period:
+
+              <Input className="rup__missue__action__ng__start-date">
+                <input
+                  type="text"
+                  ref={this.setStartDateRef}
+                />
+              </Input>
+              -
+              <Input className="rup__missue__action__ng__end-date">
+                <input
+                  type="text"
+                  ref={this.setEndDateRef}
+                />
+              </Input>
+            </div>
           </div>
           <div className="rup__missue__action__ellipsis">
             <Dropdown
@@ -116,7 +207,7 @@ class EditableMinisterIssueActionBox extends Component {
             rows={3}
             placeholder={detailPlaceholder}
             onChange={this.onActionFieldChanged}
-            value={detail}
+            value={detail || ''}
             style={{ marginTop: '10px' }}
           />
           <span className="rup__missue__action__detail-asterisk">
