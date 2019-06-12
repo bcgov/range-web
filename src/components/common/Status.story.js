f25a6dae105c71b49f55d0d158395109766c1b6c
twostoryrobotrange <range@twostoryrobot.com>
2019-06-11 17:11:45 -0700

    added story for Status

---
+++ src/components/common/Status.story.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import Status from './Status'
+import { PLAN_STATUS } from '../../constants/variables'
+import { USER_ROLE } from '../../constants/variables'
+
+import { storiesOf } from '@storybook/react'
+import { withKnobs, select } from '@storybook/addon-knobs'
+
+let statusPropOptions = {}
+const statusSelectOptions = Object.keys(PLAN_STATUS)
+statusSelectOptions.forEach(
+  status => (statusPropOptions[status] = { code: PLAN_STATUS[status] })
+)
+
+const userPropOptions = {
+  'Agreement Holder': { roles: [USER_ROLE.AGREEMENT_HOLDER] },
+  'Not Agreement Holder': { roles: [] }
+}
+
+storiesOf('Status', module)
+  .addDecorator(withKnobs)
+  .add('With knobs', () => (
+    <Status
+      user={select(
+        'User',
+        userPropOptions,
+        userPropOptions['Agreement Holder'],
+        'User'
+      )}
+      status={select(
+        'Plan Status',
+        statusPropOptions,
+        { code: 'SFD' },
+        'Plan Status'
+      )}
+    />
+  ))
