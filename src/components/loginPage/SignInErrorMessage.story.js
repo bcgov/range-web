16cb9e4e9c6795604d52877a18b50249aa615de3
twostoryrobotrange <range@twostoryrobot.com>
2019-06-11 17:11:44 -0700

    SignInErrorMessage stories

---
+++ src/components/loginPage/SignInErrorMessage.story.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import SignInErrorMessage from './SignInErrorMessage'
+
+const stories = storiesOf('SignInErrorMessage', module)
+stories.add('Default', () => <SignInErrorMessage />)
+stories.add('error fetching user', () => (
+  <SignInErrorMessage
+    errorFetchingUser={{ status: 403, data: { error: 'error message' } }}
+    signOut={action('sign out clicked')}
+  />
+))
+stories.add('non 403 error', () => (
+  <SignInErrorMessage
+    errorFetchingUser={{ status: 404, data: { error: 'error message' } }}
+  />
+))
