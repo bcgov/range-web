156ebab60d4c4a2b9f8fa289598a2edecac5771c
twostoryrobotrange <range@twostoryrobot.com>
2019-06-11 17:11:44 -0700

    simple story for BrowserWarningHeader

---
+++ src/components/loginPage/BrowserWarningHeader.story.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+
+import BrowserWarningHeader from './BrowserWarningHeader'
+
+storiesOf('BrowserWarningHeader', module).add('Default', () => (
+  <BrowserWarningHeader />
+))
