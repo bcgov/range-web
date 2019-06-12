a40aa154203988b8bc3860aa0a04fbfdb2b94bd8
twostoryrobotrange <range@twostoryrobot.com>
2019-06-11 17:11:44 -0700

    Avatar can take a user prop

--- src/components/common/Avatar.story.js
+++ src/components/common/Avatar.story.js
@@ -4,4 +4,8 @@ import { storiesOf } from '@storybook/react'
 
 import Avatar from './Avatar'
 
-storiesOf('Avatar', module).add('Default', () => <Avatar />)
+storiesOf('Avatar', module)
+  .add('Default', () => <Avatar />)
+  .add('with user prop', () => (
+    <Avatar user={{ familyName: 'Jones', givenName: 'Jenny' }} />
+  ))
