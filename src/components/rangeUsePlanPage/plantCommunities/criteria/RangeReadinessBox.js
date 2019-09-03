833d643c4d72e81fb14227f4d07caa85964eeffe
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:03:48 -0700

    Make RangeReadinessBox editable

--- src/components/rangeUsePlanPage/plantCommunities/criteria/RangeReadinessBox.js
+++ src/components/rangeUsePlanPage/plantCommunities/criteria/RangeReadinessBox.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import {
-  handleNullValue,
-  getRangeReadinessMonthAndDate
-} from '../../../../utils'
+
 import IndicatorPlantsForm from '../IndicatorPlantsForm'
-import { plantCommunityUpdated } from '../../../../actions'
 import { PLANT_CRITERIA } from '../../../../constants/variables'
+import { RANGE_READINESS } from '../../../../constants/fields'
+import PermissionsField from '../../../common/PermissionsField'
+import DateInputField from '../../../common/form/DateInputField'
+import { TextArea } from 'formik-semantic-ui'
 
 const RangeReadinessBox = ({ plantCommunity, namespace }) => {
-  const {
-    rangeReadinessDay: day,
-    rangeReadinessMonth: month,
-    rangeReadinessNote
-  } = plantCommunity
-
-  const readinessMonthAndDate = getRangeReadinessMonthAndDate(month, day)
+  const { rangeReadinessDate, rangeReadinessNotes } = plantCommunity
 
   return (
     <div className="rup__plant-community__sh">
@@ -28,10 +21,21 @@ const RangeReadinessBox = ({ plantCommunity, namespace }) => {
         If more than one readiness criteria is provided, all such criteria must
         be met before grazing may occur.
       </div>
-
-      <div className="rup__plant-community__rr__label">Readiness Date</div>
-      <div>{handleNullValue(readinessMonthAndDate)}</div>
-
+      <PermissionsField
+        name={`${namespace}.rangeReadinessDate`}
+        permission={RANGE_READINESS.DATE}
+        component={DateInputField}
+        displayValue={rangeReadinessDate}
+        label="Readiness Date"
+        dateFormat="MMMM DD"
+      />
+      <PermissionsField
+        name={`${namespace}.rangeReadinessNotes`}
+        permission={RANGE_READINESS.NOTES}
+        component={TextArea}
+        displayValue={rangeReadinessNotes}
+        label="Notes"
+      />
       <IndicatorPlantsForm
         indicatorPlants={plantCommunity.indicatorPlants.filter(
           ip => ip.criteria === PLANT_CRITERIA.RANGE_READINESS
@@ -40,9 +44,6 @@ const RangeReadinessBox = ({ plantCommunity, namespace }) => {
         valueLabel="Criteria (Leaf Stage)"
         criteria={PLANT_CRITERIA.RANGE_READINESS}
       />
-
-      <div className="rup__plant-community__rr__label">Notes</div>
-      <div>{handleNullValue(rangeReadinessNote)}</div>
     </div>
   )
 }
@@ -61,11 +62,4 @@ RangeReadinessBox.propTypes = {
   namespace: PropTypes.string.isRequired
 }
 
-const mapDispatchToProps = {
-  plantCommunityUpdated
-}
-
-export default connect(
-  null,
-  mapDispatchToProps
-)(RangeReadinessBox)
+export default RangeReadinessBox
