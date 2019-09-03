03a2fcee8d90bd19c9a2a540b0ecd3f94b7ac968
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:04:05 -0700

    Allow DateInputField to be inline

--- src/components/common/form/DateInputField.js
+++ src/components/common/form/DateInputField.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import { DateInput } from 'semantic-ui-calendar-react'
 import { Field } from 'formik'
 
-const DateInputField = props => (
+const DateInputField = ({ inline, ...props }) => (
   <Field
     {...props}
+    inline={inline}
     render={({ field, form }) => (
       <DateInput
         {...field}
         {...props}
+        inlineLabel={inline}
         onChange={(e, { value }) => {
           form.setFieldValue(field.name, value)
         }}
@@ -16,5 +19,9 @@ const DateInputField = props => (
       />
     )}></Field>
 )
+
+DateInputField.propTypes = {
+  inline: PropTypes.bool
+}
 
 export default DateInputField
