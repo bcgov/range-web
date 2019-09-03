96a184476fba63a8090354be092b2652ba79890c
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:03:59 -0700

    Add PlantCommunityBox story

---
+++ src/components/rangeUsePlanPage/plantCommunities/PlantCommunityBox.story.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { Form } from 'formik-semantic-ui'
+import { action } from '@storybook/addon-actions'
+
+import PlantCommunityBox from './PlantCommunityBox'
+
+const plantCommunity = {
+  name: 'Plant Community',
+  plantCommunityActions: [],
+  purposeOfAction: '',
+  aspect: '',
+  elevation: 0,
+  url: '',
+  approved: '',
+  notes: '',
+  communityType: { name: 'My community' },
+  monitoringAreas: [],
+  indicatorPlants: []
+}
+
+storiesOf('rangeUsePlanPage/plantCommunities/PlantCommunityBox', module).add(
+  'default',
+  () => (
+    <Form
+      initialValues={{ plantCommunity }}
+      render={({ values }) => (
+        <ul className="collaspible-boxes">
+          <PlantCommunityBox
+            namespace="plantCommunity"
+            plantCommunity={values.plantCommunity}
+            onClick={action('click')}
+            index={1}
+            activeIndex={1}
+          />
+        </ul>
+      )}
+    />
+  )
+)
