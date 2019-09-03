7674c60950a96295e789776a3fe257ddc301c9d8
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:04:30 -0700

    Refactor notifications

--- src/components/rangeUsePlanPage/notifications/index.js
+++ src/components/rangeUsePlanPage/notifications/index.js
@@ -7,49 +7,40 @@ import {
   isUserStaff
 } from '../../../utils'
 import AHSignaturesStatusModal from './AHSignaturesStatusModal'
+import { useUser } from '../../../providers/UserProvider'
 
-class Notifications extends Component {
-  static propTypes = {
-    plan: PropTypes.shape({}).isRequired,
-    user: PropTypes.shape({}).isRequired,
-    references: PropTypes.shape({}).isRequired,
-    confirmationsMap: PropTypes.shape({}).isRequired,
-    planStatusHistoryMap: PropTypes.shape({}).isRequired,
-    planTypeDescription: PropTypes.string
-  }
+const Notifications = ({ plan, planTypeDescription = '' }) => {
+  const user = useUser()
 
-  static defaultProps = {
-    planTypeDescription: ''
-  }
+  const { status, planStatusHistory } = plan
 
-  render() {
-    const { plan, user, planTypeDescription } = this.props
-    const { status, planStatusHistory } = plan
-
-    return (
-      <div className="rup__notifications">
-        {isUserStaff(user) && isStatusIndicatingStaffFeedbackNeeded(status) && (
-          <div className="rup__feedback-notification">
-            <div className="rup__feedback-notification__title">
-              {`Provide input for ${planTypeDescription} Submission`}
-            </div>
-            Review the Range Use Plan and provide for feedback
+  return (
+    <div className="rup__notifications">
+      {isUserStaff(user) && isStatusIndicatingStaffFeedbackNeeded(status) && (
+        <div className="rup__feedback-notification">
+          <div className="rup__feedback-notification__title">
+            {`Provide input for ${planTypeDescription} Submission`}
           </div>
-        )}
+          Review the Range Use Plan and provide for feedback
+        </div>
+      )}
 
-        {planStatusHistory.length !== 0 && (
-          <StatusHistory
-            {...this.props}
-            planStatusHistory={planStatusHistory}
-          />
-        )}
+      {planStatusHistory.length !== 0 && (
+        <StatusHistory planStatusHistory={planStatusHistory} />
+      )}
 
-        {isStatusAwaitingConfirmation(status) && (
-          <AHSignaturesStatusModal {...this.props} />
-        )}
-      </div>
-    )
-  }
+      {isStatusAwaitingConfirmation(status) && (
+        <AHSignaturesStatusModal {...this.props} />
+      )}
+    </div>
+  )
+}
+
+Notifications.propTypes = {
+  plan: PropTypes.shape({}).isRequired,
+  confirmationsMap: PropTypes.shape({}).isRequired,
+  planStatusHistoryMap: PropTypes.shape({}).isRequired,
+  planTypeDescription: PropTypes.string
 }
 
 export default Notifications
