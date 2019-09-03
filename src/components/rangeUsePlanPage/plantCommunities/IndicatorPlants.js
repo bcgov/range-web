2881072b07883c0e30370c99090cac51217daebb
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:03:22 -0700

    Refactor IndicatorPlants component

--- src/components/rangeUsePlanPage/plantCommunities/IndicatorPlants.js
+++ src/components/rangeUsePlanPage/plantCommunities/IndicatorPlants.js
@@ -1,44 +1,42 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { NOT_PROVIDED } from '../../../constants/strings'
 import { handleNullValue } from '../../../utils'
 
-class IndicatorPlants extends Component {
-  static propTypes = {
-    indicatorPlants: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-    criteria: PropTypes.string.isRequired
-  }
+const IndicatorPlants = ({ indicatorPlants = [], criteria }) => {
+  const filtered = indicatorPlants.filter(ip => ip.criteria === criteria)
 
-  renderIndicatorPlant = indicatorPlant => {
-    const { id, plantSpecies, value } = indicatorPlant
-    const plantSpeciesName = plantSpecies && plantSpecies.name
-
+  if (filtered.length === 0) {
     return (
-      <div key={id} className="rup__plant-community__i-plant__row">
-        <div>{handleNullValue(plantSpeciesName)}</div>
-        <div>{handleNullValue(value)}</div>
+      <div className="rup__plant-community__i-plants__not-provided">
+        {NOT_PROVIDED}
       </div>
     )
   }
 
-  render() {
-    const { indicatorPlants = [], criteria } = this.props
-    const filtered = indicatorPlants.filter(ip => ip.criteria === criteria)
-
-    if (filtered.length === 0) {
-      return (
-        <div className="rup__plant-community__i-plants__not-provided">
-          {NOT_PROVIDED}
+  return (
+    <div className="rup__plant-community__i-plants">
+      {filtered.map(plant => (
+        <div key={plant.id} className="rup__plant-community__i-plant__row">
+          <div>
+            {handleNullValue(plant.plantSpecies && plant.plantSpecies.name)}
+          </div>
+          <div>{handleNullValue(plant.value)}</div>
         </div>
-      )
-    }
+      ))}
+    </div>
+  )
+}
 
-    return (
-      <div className="rup__plant-community__i-plants">
-        {filtered.map(this.renderIndicatorPlant)}
-      </div>
-    )
-  }
+IndicatorPlants.propTypes = {
+  indicatorPlants: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      plantSpecies: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    })
+  ),
+  criteria: PropTypes.string.isRequired
 }
 
 export default IndicatorPlants
