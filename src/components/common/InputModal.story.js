e94c0da04e77741388aae6465e05e2853ef7d2fc
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:03:57 -0700

    Add InputModal story

---
+++ src/components/common/InputModal.story.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+
+import InputModal from './InputModal'
+
+storiesOf('InputModal', module).add('default', () => (
+  <InputModal
+    open={true}
+    onSubmit={action('submit')}
+    onClose={action('close')}
+    title="My Input Modal"
+  />
+))
