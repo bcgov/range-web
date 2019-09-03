82b59fc683f283f589e8b3cf76439cb28841840f
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:03:26 -0700

    Wrap app in providers

--- src/components/App.js
+++ src/components/App.js
@@ -21,6 +21,8 @@ import React, { Component } from 'react'
 import { Provider } from 'react-redux'
 import configureStore from '../configureStore'
 import Router from './router'
+import UserProvider from '../providers/UserProvider'
+import ReferencesProvider from '../providers/ReferencesProvider'
 
 const store = configureStore()
 
@@ -28,7 +30,11 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Router />
+        <UserProvider>
+          <ReferencesProvider>
+            <Router />
+          </ReferencesProvider>
+        </UserProvider>
       </Provider>
     )
   }
