f7aec28953cf7263d23ec0f1844ed70c915facd3
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:04:15 -0700

    PlanForm form is moved up

--- src/components/rangeUsePlanPage/PlanForm.js
+++ src/components/rangeUsePlanPage/PlanForm.js
@@ -1,29 +1,17 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import debounce from 'lodash.debounce'
 import Pastures from './pastures'
-import { Form } from 'formik-semantic-ui'
 import { ELEMENT_ID } from '../../constants/variables'
-import Effect from '../common/form/Effect'
 import BasicInformation from './basicInformation'
+import GrazingSchedules from './grazingSchedulesRefactored'
 
 const PlanForm = ({ plan }) => {
-  const onChange = debounce(values => {
-    console.log('form change', values)
-  }, 1000)
-
   return (
-    <Form
-      initialValues={plan}
-      validateOnChange={true}
-      render={({ values: { pastures }, errors }) => (
-        <>
-          <Effect onChange={onChange} />
-          <BasicInformation plan={plan} agreement={plan.agreement} />
-          <Pastures pastures={pastures} elementId={ELEMENT_ID.PASTURES} />
-        </>
-      )}
-    />
+    <>
+      <BasicInformation plan={plan} agreement={plan.agreement} />
+      <Pastures pastures={plan.pastures} elementId={ELEMENT_ID.PASTURES} />
+      <GrazingSchedules plan={plan} />
+    </>
   )
 }
 
