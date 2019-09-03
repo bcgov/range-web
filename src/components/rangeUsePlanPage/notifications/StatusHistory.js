b9f5091b6c59dc075ee3736f577faaf561106967
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:04:15 -0700

    Make components compatible with new plan format

--- src/components/rangeUsePlanPage/notifications/StatusHistory.js
+++ src/components/rangeUsePlanPage/notifications/StatusHistory.js
@@ -7,53 +7,48 @@ import { REFERENCE_KEY } from '../../../constants/variables'
 
 class StatusHistory extends Component {
   static propTypes = {
-    planStatusHistory: PropTypes.arrayOf(PropTypes.number).isRequired,
-    planStatusHistoryMap: PropTypes.shape({}).isRequired,
+    planStatusHistory: PropTypes.arrayOf(PropTypes.object).isRequired,
     user: PropTypes.shape({}).isRequired,
     references: PropTypes.shape({}).isRequired
   }
 
   render() {
-    const {
-      planStatusHistory,
-      planStatusHistoryMap,
-      references,
-      user
-    } = this.props
+    const { planStatusHistory, references, user } = this.props
     const planStatuses = references[REFERENCE_KEY.PLAN_STATUS]
 
     return (
       <div className="rup__history">
-        {planStatusHistory.map(pshId => {
-          const record = planStatusHistoryMap[pshId]
-          const {
+        {planStatusHistory.map(
+          ({
             id,
             user: recorder,
             createdAt,
             fromPlanStatusId,
-            toPlanStatusId
-          } = record || {}
-          const from = planStatuses.find(s => s.id === fromPlanStatusId)
-          const to = planStatuses.find(s => s.id === toPlanStatusId)
+            toPlanStatusId,
+            note
+          }) => {
+            const from = planStatuses.find(s => s.id === fromPlanStatusId)
+            const to = planStatuses.find(s => s.id === toPlanStatusId)
 
-          return (
-            <div key={id} className="rup__history__record">
-              <div className="rup__history__record__header">
-                <Icon name="user circle" />
-                {getUserFullName(recorder)}
-                <div className="rup__history__record__timestamp">
-                  {formatDateToNow(createdAt)}
+            return (
+              <div key={id} className="rup__history__record">
+                <div className="rup__history__record__header">
+                  <Icon name="user circle" />
+                  {getUserFullName(recorder)}
+                  <div className="rup__history__record__timestamp">
+                    {formatDateToNow(createdAt)}
+                  </div>
                 </div>
+                <div className="rup__history__record__statuses">
+                  <Status status={from} user={user} />
+                  <Icon name="long arrow alternate right" size="large" />
+                  <Status status={to} user={user} />
+                </div>
+                {note}
               </div>
-              <div className="rup__history__record__statuses">
-                <Status status={from} user={user} />
-                <Icon name="long arrow alternate right" size="large" />
-                <Status status={to} user={user} />
-              </div>
-              {record.note}
-            </div>
-          )
-        })}
+            )
+          }
+        )}
       </div>
     )
   }
