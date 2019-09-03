59852a649f3df76240fdd0d144d324dbae9fcdbe
twostoryrobotrange <range@twostoryrobot.com>
2019-09-03 15:03:25 -0700

    Wrap stories with mocked providers

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,31 @@
-import { configure } from '@storybook/react'
+import React from 'react'
+import { configure, addDecorator } from '@storybook/react'
 
 // import the global styles
 import '../src/semantic/semantic.min.css'
 import '../src/styles/index.scss'
+
+import { UserContext } from '../src/providers/UserProvider'
+import { ReferencesContext } from '../src/providers/ReferencesProvider'
+import mockReferences from './mocks/references'
+import mockUser from './mocks/user'
 
 // automatically import all files ending in *.stories.js or *.story.js
 const req = require.context('../src/components', true, /\.(stories|story)\.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
+
+// Mock user
+addDecorator(story => (
+  <UserContext.Provider value={mockUser}>{story()}</UserContext.Provider>
+))
+
+// Mock references
+addDecorator(story => (
+  <ReferencesContext.Provider value={mockReferences}>
+    {story()}
+  </ReferencesContext.Provider>
+))
 
 configure(loadStories, module)
---
+++ .storybook/mocks/user.js
@@ -0,0 +1,13 @@
+export default {
+  id: 66,
+  username: 'range@twostoryrobot.com',
+  clientId: null,
+  givenName: 'Ranger',
+  familyName: 'Smith',
+  email: 'range@twostoryrobot.com',
+  phoneNumber: null,
+  active: true,
+  piaSeen: true,
+  lastLoginAt: '2019-08-13T22:01:52.224Z',
+  roles: ['myra_range_officer']
+}
