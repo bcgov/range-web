8d214b205c4dde86133af8f263138c7255be958f
Caleb Sharp <epicsharp11@gmail.com>
2019-09-20 15:47:12 -0700

    Use regular datepicker in DayMonthPicker

--- src/components/common/form/DayMonthPicker.js
+++ src/components/common/form/DayMonthPicker.js
@@ -1,81 +1,30 @@
 import React from 'react'
-import { Form } from 'semantic-ui-react'
-import { Field, getIn } from 'formik'
+import { getIn, connect } from 'formik'
 import moment from 'moment'
+import { DateInput } from 'semantic-ui-calendar-react'
 
-const DayMonthPicker = ({ dayName, monthName, label }) => {
-  const monthOptions = moment.months().map((month, i) => ({
-    text: month,
-    value: i + 1,
-    key: month
-  }))
-
-  const dayOptionsForMonth = month => {
-    if (!month || month < 1 || month > 12) return []
-    const days = moment(
-      monthOptions.find(m => m.value === month).text + ' ' + moment().year()
-    ).daysInMonth()
-
-    const options = Array.from({ length: days }).map((_, i) => ({
-      value: i + 1,
-      text: i + 1,
-      key: i + 1
-    }))
-
-    return options
-  }
-
-  const monthId = `field_input_${monthName}`
-  const dayId = `field_input_${dayName}`
-
+const DayMonthPicker = ({ dayName, monthName, label, formik, ...props }) => {
   return (
     <>
-      <Field
-        name={monthName}
-        render={({ field, form }) => (
-          <Form.Field>
-            {!!label && <label htmlFor={monthId}>{label}</label>}
-            <Form.Dropdown
-              id={monthId}
-              name={monthName}
-              placeholder="Month"
-              options={monthOptions}
-              onChange={(e, { name, value }) => {
-                form.setFieldValue(name, value)
-              }}
-              selection
-              fluid
-              value={field.value}
-            />
-          </Form.Field>
-        )}
-      />
+      <DateInput
+        onChange={(e, { value }) => {
+          const date = moment(value, 'MMMM Do')
+          const month = date.month() + 1
+          const day = date.date()
 
-      <Field
-        name={dayName}
-        render={({ field, form }) => {
-          return (
-            <Form.Field>
-              {!!label && <label htmlFor={dayId}>&nbsp;</label>}
-              <Form.Dropdown
-                id={dayId}
-                name={dayName}
-                placeholder="Day"
-                disabled={!getIn(form.values, monthName)}
-                options={dayOptionsForMonth(getIn(form.values, monthName))}
-                onChange={(e, { name, value }) => {
-                  form.setFieldValue(name, value)
-                }}
-                selection
-                fluid
-                value={field.value}
-              />
-            </Form.Field>
-          )
+          console.log(month, day)
+          formik.setFieldValue(monthName, month)
+          formik.setFieldValue(dayName, day)
         }}
+        value={`${moment(getIn(formik.values, monthName), 'MM').format(
+          'MMMM'
+        )} ${moment(getIn(formik.values, dayName), 'DD').format('Do')} `}
+        dateFormat={'MMMM Do'}
+        label={label}
+        {...props}
       />
     </>
   )
 }
 
-export default DayMonthPicker
+export default connect(DayMonthPicker)
