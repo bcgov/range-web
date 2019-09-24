a2f43806a3603d49b0fe0df620724be35e0a0f22
Caleb Sharp <epicsharp11@gmail.com>
2019-09-24 08:21:12 -0700

    Use schema in story

--- src/components/rangeUsePlanPage/additionalRequirements/index.story.js
+++ src/components/rangeUsePlanPage/additionalRequirements/index.story.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 import { Form } from 'formik-semantic-ui'
+import schema from '../schema'
 
 import AdditionalRequirements from '.'
 
@@ -9,19 +10,19 @@ const additionalRequirements = [
     id: 1,
     detail: 'Details, details.',
     url: 'http://example.com',
-    category: 4
+    categoryId: 4
   },
   {
     id: 2,
     detail: 'Lorem ipsum something something',
     url: 'http://example.com',
-    category: 3
+    categoryId: 3
   },
   {
     id: 3,
     detail: 'bęéëēp',
     url: 'http://example.com',
-    category: 3
+    categoryId: 3
   }
 ]
 
@@ -29,13 +30,18 @@ storiesOf('rangeUsePlanPage/additionalRequirements', module).add(
   'default',
   () => (
     <Form
+      validationSchema={schema}
+      validateOnChange
       initialValues={{
         additionalRequirements
       }}
       render={({ values }) => (
-        <AdditionalRequirements
-          additionalRequirements={values.additionalRequirements}
-        />
+        <>
+          <button type="submit">Submit</button>
+          <AdditionalRequirements
+            additionalRequirements={values.additionalRequirements}
+          />
+        </>
       )}
     />
   )
