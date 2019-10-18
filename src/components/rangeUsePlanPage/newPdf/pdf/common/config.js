aecb884b280ab56a7764e9386256430012f12a9a
Caleb Sharp <epicsharp11@gmail.com>
2019-10-18 15:59:00 -0700

    Add pdf config and fonts

---
+++ src/components/rangeUsePlanPage/newPdf/pdf/common/config.js
@@ -0,0 +1,13 @@
+export const config = {
+  primaryColor: '#002C71',
+  primaryRGB: [0, 44, 113],
+  accentColor: '#F3B229',
+  blackColor: '#000000',
+  grayColor: '#444444',
+  lightGrayColor: '#777777',
+  sectionTitleFontSize: 13,
+  fieldTitleFontSize: 11,
+  normalFontSize: 10,
+  fontFamily: 'Lato',
+  dateFormat: 'MMMM DD, YYYY'
+}
