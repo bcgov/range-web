cb4cbb18ed6bc2f5f80108f1fe0ff899a36e11df
Caleb Sharp <epicsharp11@gmail.com>
2019-11-15 13:37:52 -0800

    Handle if no pasture is set in schedule entry row

--- src/components/rangeUsePlanPage/grazingSchedules/GrazingScheduleEntryRow.js
+++ src/components/rangeUsePlanPage/grazingSchedules/GrazingScheduleEntryRow.js
@@ -152,7 +152,7 @@ const GrazingScheduleEntryRow = ({
         <PermissionsField
           permission={SCHEDULE.GRACE_DAYS}
           name={`${namespace}.graceDays`}
-          displayValue={graceDays || pasture.graceDays}
+          displayValue={graceDays || (pasture ? pasture.graceDays : '')}
           inputProps={{
             type: 'number',
             fluid: true,
