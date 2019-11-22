9208c69fa1c820bd117b106a611089c6cd281cec
Caleb Sharp <epicsharp11@gmail.com>
2019-11-22 11:26:26 -0800

    Wrap plan form in editable provider

--- src/components/rangeUsePlanPage/PlanForm.js
+++ src/components/rangeUsePlanPage/PlanForm.js
@@ -10,10 +10,16 @@ import InvasivePlantChecklist from './invasivePlantChecklist'
 import ManagementConsiderations from './managementConsiderations'
 import MinisterIssues from './ministerIssues'
 import AdditionalRequirements from './additionalRequirements'
+import { canUserEditThisPlan } from '../../utils'
+import { useUser } from '../../providers/UserProvider'
+import EditableProvider from '../../providers/EditableProvider'
 
 const PlanForm = ({ plan }) => {
+  const user = useUser()
+  const isEditable = canUserEditThisPlan(plan, user)
+
   return (
-    <>
+    <EditableProvider editable={isEditable}>
       <Element name={ELEMENT_ID.BASIC_INFORMATION}>
         <BasicInformation plan={plan} agreement={plan.agreement} />
       </Element>
@@ -48,7 +54,7 @@ const PlanForm = ({ plan }) => {
           managementConsiderations={plan.managementConsiderations}
         />
       </Element>
-    </>
+    </EditableProvider>
   )
 }
 
