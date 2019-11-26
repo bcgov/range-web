63a5aa5ea1a251b38f47bfb107e983718c641404
Caleb Sharp <epicsharp11@gmail.com>
2019-11-26 13:17:14 -0800

    Create dropdown component that can display help text

---
+++ src/components/common/form/HelpfulDropdown.story.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import HelpfulDropdown from './HelpfulDropdown'
+import { Form } from 'formik-semantic-ui'
+
+export default {
+  title: 'HelpfulDropdown'
+}
+
+const options = [
+  { value: 'en', key: 'en', text: 'English' },
+  { value: 'es', key: 'es', text: 'Spanish' },
+  { value: 'fr', key: 'fr', text: 'French' }
+]
+
+export const withInfoText = () => (
+  <Form
+    initialValues={{ language: 'fr' }}
+    render={() => (
+      <HelpfulDropdown
+        help="This is the info for the dropdown"
+        label="Language"
+        options={options}
+        name="language"
+      />
+    )}
+  />
+)
