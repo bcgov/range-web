de40279e9dd64b329efe6afddfec2c5a0bd59ebb
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-01-30 12:01:17 -0800

    Update tests

--- src/components/common/MultiParagraphDisplay.js
+++ src/components/common/MultiParagraphDisplay.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const MultiParagraphDisplay = ({ value }) =>
-  value
-    .toString()
-    .split('\n')
-    .map((line, i) => <p key={i}>{line}</p>)
+const MultiParagraphDisplay = ({ value, ...props }) => (
+  <>
+    <input type="hidden" value={value} {...props} />
+    <div>
+      {value
+        .toString()
+        .split('\n')
+        .map((line, i) => (
+          <p key={i}>{line}</p>
+        ))}
+    </div>
+  </>
+)
 
 export default MultiParagraphDisplay
