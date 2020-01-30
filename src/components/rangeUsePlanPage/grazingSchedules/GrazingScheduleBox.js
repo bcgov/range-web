fa57e29463b2b9bb905b2cc08d38baac763430f9
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-01-30 14:36:59 -0800

    Reset sort order after clicking header twice

--- src/components/rangeUsePlanPage/grazingSchedules/GrazingScheduleBox.js
+++ src/components/rangeUsePlanPage/grazingSchedules/GrazingScheduleBox.js
@@ -79,13 +79,21 @@ const GrazingScheduleBox = ({
         orderByColumn('asc', schedule.grazingScheduleEntries)
       )
     } else {
-      const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc'
-      setSortOrder(newSortOrder)
+      if (sortOrder === 'asc') {
+        setSortOrder('desc')
 
-      formik.setFieldValue(
-        `${namespace}.grazingScheduleEntries`,
-        orderByColumn(newSortOrder, schedule.grazingScheduleEntries)
-      )
+        formik.setFieldValue(
+          `${namespace}.grazingScheduleEntries`,
+          orderByColumn('desc', schedule.grazingScheduleEntries)
+        )
+      } else {
+        setSortOrder(null)
+        setSortBy(null)
+        formik.setFieldValue(
+          `${namespace}.grazingScheduleEntries`,
+          _.orderBy('createdAtDate', 'asc', schedule.grazingScheduleEntries)
+        )
+      }
     }
   }
 
