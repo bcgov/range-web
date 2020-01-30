534f28fe6631a67a2a8d70bf92ab719d3fe81c9c
micheal-w-wells <micheal.w.wells@gmail.com>
2020-01-29 17:52:59 -0800

    remove alert

--- src/index.js
+++ src/index.js
@@ -32,7 +32,6 @@ import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<App />, document.getElementById('root'))
 
-alert('yes it actually deployed')
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
