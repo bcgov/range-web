df7bec0889690a0b13e377e7056adb2e3ab96256
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-02-04 15:37:24 -0800

    Create formik wrapped react-select component

---
+++ src/components/common/Select.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactSelect from 'react-select'
+import { connect, getIn } from 'formik'
+
+const Select = ({ name, formik, onChange, options = [], inputProps }) => {
+  const currentValue = getIn(formik.values, name)
+  const selectedOption = options.find(f => f.value === currentValue)
+
+  return (
+    <ReactSelect
+      {...inputProps}
+      name={name}
+      options={options}
+      value={selectedOption}
+      onChange={({ value }) => {
+        formik.setFieldValue(name, value)
+        if (onChange) {
+          onChange(value)
+        }
+      }}
+      isSearchable
+      styles={{
+        container: styles => ({
+          ...styles,
+          width: '170px'
+        })
+      }}
+      menuPortalTarget={document.body}
+    />
+  )
+}
+
+export default connect(Select)
