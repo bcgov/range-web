63a6e66313d883b59ecac5183c74ccd069ac6682
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-02-27 14:20:10 -0800

    Replace react-loadable with loadable-components

--- src/components/router/LoadableComponent.js
+++ src/components/router/LoadableComponent.js
@@ -1,29 +1,12 @@
 import React from 'react'
-import Loadable from 'react-loadable'
-import { Loading, ErrorPage } from '../common'
+import loadable from '@loadable/component'
+import { Loading } from '../common'
 
 /*
   Code Splitting with React Router
   https://serverless-stack.com/chapters/code-splitting-in-create-react-app.html
 */
-
-const LoadingComponent = ({ isLoading, error }) => {
-  let component = <div />
-  if (isLoading) {
-    // Handle the loading state
-    component = <Loading />
-  } else if (error) {
-    // Handle the error state
-    component = (
-      <ErrorPage message="Sorry, there was a problem loading the page." />
-    )
-  }
-
-  return component
-}
-
-export const LoadableComponent = Component =>
-  Loadable({
-    loader: Component,
-    loading: LoadingComponent
+export const LoadableComponent = cb =>
+  loadable(cb, {
+    fallback: <Loading />
   })
