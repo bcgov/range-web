058960f346ab09b20c0a91f862e89deefee13613
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-04-16 08:10:39 -0700

    Update manage client page tests

--- src/tests/intergration/ManageClientPage.test.js
+++ src/tests/intergration/ManageClientPage.test.js
@@ -1,51 +1,135 @@
 import React from 'react'
 import MockAdapter from 'axios-mock-adapter'
-import { Provider } from 'react-redux'
-import { mount } from 'enzyme'
-import { MemoryRouter, withRouter } from 'react-router-dom'
-import thunk from 'redux-thunk'
-import { axios } from '../../utils'
-import ManageClientPage from '../../components/manageClientPage'
-import { storeAuthData, storeReferences } from '../../actions'
-import { configureMockStore, flushAllPromises } from '../helpers/utils'
+import { groupBy } from 'lodash'
 import {
-  requestMockHeader,
-  mockUsers,
-  mockReference,
-  mockAuthData
-} from './mockData'
+  render,
+  queryAllByText as globalQueryAllByText,
+  queryByText as globalQueryByText,
+  waitFor
+} from '../helpers/test-utils'
+import userEvent from '@testing-library/user-event'
+import { MemoryRouter, withRouter } from 'react-router-dom'
+import { axios, getUserFullName } from '../../utils'
+import ManageClientPage from '../../components/manageClientPage'
+import { mockUsers, mockClients } from './mockData'
 import * as API from '../../constants/api'
 
-let store
+// Group mock users by full name
+const usersByName = groupBy(mockUsers, user => getUserFullName(user))
+
 const mockAxios = new MockAdapter(axios)
 
+// global.document.createRange = () => ({
+//   setStart: () => {},
+//   setEnd: () => {},
+//   commonAncestorContainer: {
+//     nodeName: 'BODY',
+//     ownerDocument: document
+//   }
+// })
+
 beforeEach(() => {
-  store = configureMockStore([thunk])
-  store.dispatch(storeAuthData(mockAuthData))
-  store.dispatch(storeReferences(mockReference))
   mockAxios.reset()
 })
 
-describe('Integration testing', () => {
-  it('Component initializes properly', async () => {
-    const config = {
-      ...requestMockHeader(store.getState)
-    }
-
-    mockAxios.onGet(API.GET_USERS, config).reply(200, mockUsers)
+describe('Manage client page', () => {
+  it('Loads a dropdown populated with users', async () => {
+    mockAxios.onGet(new RegExp(API.GET_USERS)).reply(200, mockUsers)
 
     const ManageClientWithRouter = withRouter(ManageClientPage)
-    const wrapper = mount(
-      <Provider store={store}>
-        <MemoryRouter initialEntries={['manage-client']}>
-          <ManageClientWithRouter />
-        </MemoryRouter>
-      </Provider>
+    const { getByLabelText, getByRole } = render(
+      <MemoryRouter initialEntries={['manage-client']}>
+        <ManageClientWithRouter />
+      </MemoryRouter>
     )
 
-    await flushAllPromises()
-    wrapper.update()
+    expect(getByRole('progressbar')).toBeInTheDocument()
 
-    expect(wrapper.find('DropdownItem')).toHaveLength(mockUsers.length)
+    const userSelect = await waitFor(() => getByLabelText('Select user'))
+    expect(userSelect).toBeInTheDocument()
+
+    const openButton = await getByLabelText('Open')
+
+    userEvent.click(openButton)
+
+    const listBox = getByRole('listbox')
+
+    Object.entries(usersByName).forEach(([name, users]) => {
+      const userOptions = globalQueryAllByText(listBox, name)
+      expect(userOptions).toHaveLength(users.length)
+    })
+  })
+
+  it('Searches the list of users when you type', async () => {
+    const searchText = 'Agreement Holder'
+    const searchRegex = new RegExp(searchText)
+
+    mockAxios.onGet(new RegExp(API.GET_USERS)).reply(200, mockUsers)
+
+    const ManageClientWithRouter = withRouter(ManageClientPage)
+    const { getByLabelText, getByRole } = render(
+      <MemoryRouter initialEntries={['manage-client']}>
+        <ManageClientWithRouter />
+      </MemoryRouter>
+    )
+
+    const userSelect = await waitFor(() => getByLabelText('Select user'))
+    expect(userSelect).toBeInTheDocument()
+
+    // Search for a user
+    userEvent.type(userSelect, searchText)
+
+    const listBox = getByRole('listbox')
+
+    const userOption = globalQueryByText(listBox, searchRegex)
+    expect(userOption).toBeInTheDocument()
+
+    Object.entries(usersByName).forEach(([name]) => {
+      // Make sure only options that match the typed in text appear in the list
+      if (!name.match(searchRegex)) {
+        const userOptions = globalQueryAllByText(listBox, name)
+        expect(userOptions).toHaveLength(0)
+      } else {
+        const ahOption = globalQueryByText(listBox, name)
+        expect(ahOption).toBeInTheDocument(0)
+      }
+    })
+  })
+
+  it('Allows you to choose a user', async () => {
+    mockAxios.onGet(new RegExp(API.GET_USERS)).reply(200, mockUsers)
+    mockAxios.onGet(new RegExp(API.SEARCH_CLIENTS)).reply(200, mockClients)
+
+    const ManageClientWithRouter = withRouter(ManageClientPage)
+    const {
+      getByLabelText,
+      getAllByRole,
+      getByRole,
+      debug,
+      queryByText
+    } = render(
+      <MemoryRouter initialEntries={['manage-client']}>
+        <ManageClientWithRouter />
+      </MemoryRouter>
+    )
+
+    const userSelect = await waitFor(() => getByLabelText('Select user'))
+    expect(userSelect).toBeInTheDocument()
+    expect(userSelect).not.toHaveValue()
+
+    const openButton = await getByLabelText('Open')
+
+    userEvent.click(openButton)
+
+    const listBox = getByRole('listbox')
+
+    const userOption = globalQueryByText(listBox, /Agreement Holder 1/g)
+    expect(userOption).not.toBeNull()
+
+    userEvent.click(userOption)
+
+    await waitFor(() => {
+      expect(userSelect).toHaveValue('Agreement Holder 1 Range')
+    })
   })
 })
