80ecc4b55692d527c82a615a105f8cf82d3d3be5
Oamar Kanji <30703259+okanji@users.noreply.github.com>
2020-04-23 11:50:17 -0700

    Revert "Show legal versions"

--- src/components/rangeUsePlanPage/versionsList/VersionsToolbar.js
+++ src/components/rangeUsePlanPage/versionsList/VersionsToolbar.js
@@ -7,8 +7,6 @@ import {
   ListItemAvatar,
   Avatar
 } from '@material-ui/core'
-import Chip from '@material-ui/core/Chip'
-import { withStyles } from '@material-ui/core/styles'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 import { RANGE_USE_PLAN } from '../../../constants/routes'
@@ -45,36 +43,6 @@ const VersionsToolbar = ({
     version: v
   }))
 
-  const StyledMenuItem = withStyles(() => ({
-    root: {
-      width: 500,
-      display: 'flex',
-      flexDirection: 'row',
-      justifyContent: 'flex-start'
-    }
-  }))(MuiMenuItem)
-
-  const ApprovedChip = withStyles(() => ({
-    root: {
-      backgroundColor: '#8bc34a',
-      color: 'white'
-    }
-  }))(Chip)
-
-  const DisabledApprovedChip = withStyles(() => ({
-    root: {
-      backgroundColor: '#9e9e9e',
-      color: 'white'
-    }
-  }))(Chip)
-
-  const createApprovedChip = current =>
-    current ? (
-      <ApprovedChip label="Approved" />
-    ) : (
-      <DisabledApprovedChip label="Approved" disabled />
-    )
-
   return (
     <>
       <Menu attached="top">
@@ -89,13 +57,14 @@ const VersionsToolbar = ({
           {selectedVersion ? `v${selectedVersion.version}` : 'Select a version'}{' '}
           <Icon name="caret down" />
         </Menu.Item>
+
         <MuiMenu
           placeholder="Select a version"
           onClose={handleClose}
           open={Boolean(anchorEl)}
           anchorEl={anchorEl}>
-          {versionOptions.map((option, index) => (
-            <StyledMenuItem
+          {versionOptions.map(option => (
+            <MuiMenuItem
               key={option.key}
               selected={selectedVersion === option.value}
               onClick={e => {
@@ -103,37 +72,15 @@ const VersionsToolbar = ({
                 handleClose()
               }}>
               <ListItemAvatar>
-                <Avatar>{versionOptions.length - index}</Avatar>
+                <Avatar>v{option.version.version}</Avatar>
               </ListItemAvatar>
               <ListItemText
-                primary={
-                  <div style={{ color: 'grey', width: 200 }}>Legal Start</div>
-                }
-                secondary={
-                  <div style={{ color: 'black' }}>
-                    {moment(option.version.effectiveLegalStart).format(
-                      'MMM DD YYYY h:mm a'
-                    )}
-                  </div>
-                }
+                primary={moment(option.version.createdAt).format(
+                  'MMM DD, YYYY, h:mm:ss a'
+                )}
+                secondary={option.version.status?.name}
               />
-              <ListItemText
-                primary={
-                  <div style={{ color: 'grey', width: 200 }}>Legal End</div>
-                }
-                secondary={
-                  <div style={{ color: 'black' }}>
-                    {option.version.effectiveLegalEnd == null
-                      ? 'Present'
-                      : moment(option.version.effectiveLegalEnd).format(
-                          'MMM DD YYYY h:mm a'
-                        )}
-                  </div>
-                }
-              />
-              {option.version.status?.name === 'Approved' &&
-                createApprovedChip(option.version.effectiveLegalEnd == null)}
-            </StyledMenuItem>
+            </MuiMenuItem>
           ))}
         </MuiMenu>
 
