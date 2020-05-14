d0a83be7fa111c9adafebda8c57479f5c926442d
Oamar Kanji <oamar.kanji@twostoryrobot.com>
2020-05-14 01:36:16 -0700

    created versions preview on home screen

---
+++ src/components/rangeUsePlanPage/versionsList/VersionsDropDown.js
@@ -0,0 +1,41 @@
+import React, { useState, useRef } from 'react'
+import useSWR from 'swr'
+import * as API from '../../../constants/api'
+import { axios, getAuthHeaderConfig } from '../../../utils'
+import VersionsDropDownList from './VersionsDropDownList'
+
+const sortVersions = (a, b) => {
+  if (b.version === -1) return 1
+  if (b.version > a.version) return 1
+  if (b.version < a.version) return -1
+  return 0
+}
+
+const VersionsDropDown = ({ match, open }) => {
+  const { planId } = match.params
+  const [selectedVersion, setSelectedVersion] = useState(null)
+  const endpoint = API.GET_RUP_VERSIONS(planId)
+
+  const { data, error, isValidating } = useSWR(endpoint, key =>
+    axios.get(key, getAuthHeaderConfig()).then(res => res.data)
+  )
+
+  const { versions = [] } = data || {}
+  const formattedVersions = versions
+    .sort(sortVersions)
+    .filter(v => v.version !== -1)
+    .filter(v => v.effectiveLegalStart !== null)
+
+  if (error) return <div>Error: {JSON.stringify(error.message)}</div>
+  return (
+    <VersionsDropDownList
+      planId={planId}
+      versions={formattedVersions}
+      selectedVersion={selectedVersion}
+      open={open}
+      onSelectVersion={(e, { value }) => setSelectedVersion(value)}
+    />
+  )
+}
+
+export default VersionsDropDown
---
+++ src/components/rangeUsePlanPage/versionsList/VersionsDropdownList.js
@@ -0,0 +1,86 @@
+import React, { useState } from 'react'
+import Box from '@material-ui/core/Box'
+import Collapse from '@material-ui/core/Collapse'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Typography from '@material-ui/core/Typography'
+import moment from 'moment'
+import classnames from 'classnames'
+import Status from '../../common/Status'
+
+const VersionsDropDownList = ({ versions, open }) => {
+  const versionOptions = versions.map(v => ({
+    key: v.version,
+    value: v,
+    text: `v${v.version}`,
+    content: (
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: '40px repeat(2, auto)',
+          gridTemplateRows: '1fr'
+        }}>
+        <span>v{v.version}</span>
+        <span>{v.status.name}</span>
+        <span>{moment(v.createdAt).format('MMM DD, YYYY, h:mm:ss a')}</span>
+      </div>
+    ),
+    version: v
+  }))
+
+  return (
+    <TableRow>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={7}>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <Box margin={0} style={{ marginBottom: '10px' }}>
+            <Typography variant="h6" gutterBottom component="div">
+              Versions
+            </Typography>
+            <Table size="small" aria-label="purchases">
+              <TableHead>
+                <TableRow>
+                  <TableCell style={{ color: 'grey' }}>Legal Start</TableCell>
+                  <TableCell style={{ color: 'grey' }}>Legal End</TableCell>
+                  <TableCell style={{ color: 'grey', align: 'left' }}>
+                    Status
+                  </TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {versionOptions.map((option, index) => (
+                  <TableRow key={index} hover={true}>
+                    <TableCell component="th" scope="row">
+                      {moment(option.version.effectiveLegalStart).format(
+                        'MMM DD YYYY h:mm a'
+                      )}
+                    </TableCell>
+                    <TableCell>
+                      {option.version.effectiveLegalEnd == null
+                        ? 'Present'
+                        : moment(option.version.effectiveLegalEnd).format(
+                            'MMM DD YYYY h:mm a'
+                          )}
+                    </TableCell>
+                    <TableCell>
+                      <Status
+                        status={option.version.status}
+                        className={classnames({
+                          greyed: option.version.effectiveLegalEnd !== null
+                        })}
+                      />
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Box>
+        </Collapse>
+      </TableCell>
+    </TableRow>
+  )
+}
+
+export default VersionsDropDownList
