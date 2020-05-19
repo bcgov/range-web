60e7f9e7a2fb40d89aa93577baf9df689229f43b
Mike Wells <micheal.w.wells@gmail.com>
2020-05-19 13:37:43 -0700

    get user from context in versions toolbar

--- src/components/rangeUsePlanPage/versionsList/VersionsToolbar.js
+++ src/components/rangeUsePlanPage/versionsList/VersionsToolbar.js
@@ -15,6 +15,7 @@ import { RANGE_USE_PLAN } from '../../../constants/routes'
 import { PrimaryButton } from '../../common'
 import VersionToolbar from './VersionToolbar'
 import Status from '../../common/Status'
+import { useUser } from '../../../providers/UserProvider'
 
 const VersionsToolbar = ({
   versions,
@@ -23,6 +24,7 @@ const VersionsToolbar = ({
   planId
 }) => {
   const [anchorEl, setAnchorEl] = useState(null)
+  const user = useUser()
 
   const handleClose = () => setAnchorEl(null)
   const handleOpen = event => setAnchorEl(event.currentTarget)
@@ -129,8 +131,8 @@ const VersionsToolbar = ({
               />
               <Status
                 status={option.version.status}
-		user={user}
-		isForVersionsList={true}
+                user={user}
+                isForVersionsList={true}
                 className={classnames('versions_status_icon', {
                   greyed: option.version.isCurrentLegalVersion === false
                 })}
