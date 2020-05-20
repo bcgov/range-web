15654af2cf79b9a467d37b9c4937621ec943a16b
Oamar Kanji <oamar.kanji@twostoryrobot.com>
2020-05-20 10:56:54 -0700

    removed un used import

--- src/components/rangeUsePlanPage/versionsList/VersionsDropdownList.js
+++ src/components/rangeUsePlanPage/versionsList/VersionsDropdownList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Box from '@material-ui/core/Box'
 import Collapse from '@material-ui/core/Collapse'
 import Table from '@material-ui/core/Table'
