6d97f6a4be9b70ddc26ca170ee40f32415a74b4e
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-06-08 11:39:07 -0700

    Update login test

--- cypress/integration/newUserForm.spec.js
+++ cypress/integration/newUserForm.spec.js
@@ -1,23 +1,10 @@
 describe('Login', () => {
-  const app_base_url = Cypress.env('app_base_url')
   beforeEach(() => {
-    cy.svcClientLogout()
-    cy.svcClientLogin().as('tokens')
-    cy.get('@tokens').then(cy.svcClientSetCookie)
+    cy.login('range_officer')
   })
 
   it('Signs in, and shows privacy message on first login', () => {
-    cy.server()
-    cy.route({
-      method: 'GET',
-      url: '/api/v1/user/me'
-    }).as('me')
-
-    cy.route('/home').as('home')
-
-    cy.visit('@home')
-      .wait('@me', { responseTimeout: 15000 })
-      .then(console.log)
+    cy.visit('/home')
 
     cy.get('[class=privacy-info]').should('contain', 'Privacy') //contains('Continue to').click()
     cy.url().should('not.include', 'login')
