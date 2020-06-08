212f350b197007d1d36c47faa6ed3e266e1e1028
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-06-08 11:39:30 -0700

    Fix initial RUP test

--- cypress/integration/make_initial_RUP.spec.js
+++ cypress/integration/make_initial_RUP.spec.js
@@ -1,50 +1,92 @@
 describe('Login', () => {
-  function killSessionAndLogin(role) {
-    cy.svcClientLogout()
-    cy.getCreds(role, { log: false })
-      .then(creds => cy.svcClientLogin(...creds, { log: false }))
-      .as('tokens')
-    cy.get('@tokens').then(cy.svcClientSetCookie)
-  }
-
-  beforeEach(() => {})
-
-  it('starts a range use plan and saves + submits it', () => {
-    killSessionAndLogin('range officer')
-
-    cy.server()
-    cy.route('/home').as('home')
-
-    cy.log('go to select range use plan page')
-    cy.visit('@home')
-    cy.url().should('not.include', 'login')
-
-
-    cy.log('find the agreement')
-    cy.get('form').within(() => {
-      cy.get('input').type('RAN099931{enter}') // Only yield inputs within form
-    })
-
-    cy.log('start a RUP')
-    cy.findAllByText('New plan').click()
-    cy.url().should('not.include', 'home')
-
-    cy.log('enter a range name')
-    cy.findAllByLabelText('Range Name', { timeout: 15000 }).type(
-      'Initial RUP test'
-    )
-
-    cy.log('enter a start date')
-    cy.findAllByLabelText('Plan Start Date', {timeout: 2000}).type('2020-07-23')
-
-    cy.log('enter an end date')
-    cy.findAllByLabelText('Plan End Date', {timeout: 2000}).type('2024-06-01')
-
-    cy.log('save it')
-    cy.findAllByText('Save Draft').click()
-
-    cy.log('make sure there are no errors')
-    cy.findAllByText('Error').should('not.exist')
+  beforeEach(() => {
+    cy.login('range_officer')
   })
 
+  it('starts a range use plan and saves + submits it', () => {
+    cy.visit('/home')
+    cy.url().should('not.include', 'login')
+
+    cy.findByPlaceholderText(/Enter RAN/g).type('RAN099931{enter}')
+
+    // Create new plan
+    cy.findByText('New plan').click()
+    cy.url().should('not.include', 'home')
+
+    cy.findAllByText('Staff Draft').should('have.length', 2)
+
+    // Fill out RUP
+    cy.findByLabelText('Range Name').type('Initial RUP test')
+
+    cy.findByLabelText('Plan Start Date')
+      .parent()
+      .click()
+
+    cy.get('.ui.popup').within(() => {
+      cy.contains('.suicr-content-item', '6').click({ force: true })
+    })
+
+    cy.findByLabelText('Plan End Date')
+      .parent()
+      .click()
+
+    cy.get('.ui.popup').within(() => {
+      cy.contains('.suicr-content-item', '24').click({ force: true })
+    })
+
+    cy.findByText('Add Pasture').click()
+
+    cy.get('.ui.modals').within(() => {
+      cy.findByPlaceholderText('Pasture name').type('My pasture')
+      cy.findByText('Submit').click()
+    })
+
+    cy.findByText('Pasture: My pasture').click()
+
+    cy.findByLabelText(/Allowable AUMs/g).type('23')
+    cy.findByLabelText(/Private Land Deduction/g).type('{backspace}12')
+    cy.findByLabelText(/Pasture Notes/g).type(
+      'Here are my pasture notes{enter}A new line of notes.'
+    )
+
+    cy.findByText('Add Plant Community').click()
+    cy.findByText('Cattail').click()
+
+    cy.findAllByText('Cattail')
+      .eq(1)
+      .click()
+
+    cy.findByLabelText(/Aspect/g).type('SW')
+    cy.findByLabelText(/Elevation/g)
+      .click()
+      .contains('700-899')
+      .click()
+    cy.findByLabelText(/Approved by minister/g).click({ force: true })
+    cy.findByLabelText(/Plant Community Description/g).type(
+      'Description for my plant community'
+    )
+    cy.findByLabelText(/Community URL/g).type('https://www.google.com')
+    cy.findByLabelText(/Purpose of Action/g)
+      .click()
+      .contains(/Maintain/g)
+      .click()
+
+    // Save
+    cy.findByText('Save Draft').click()
+
+    cy.get('.sui-error-message').should('not.exist')
+    cy.findByText('Successfully saved draft').should('exist')
+
+    // Submit
+    cy.findByText('Submit').click()
+
+    cy.get('.ui.modals').within(() => {
+      cy.findByPlaceholderText('Add notes here').type('Submission notes here')
+      cy.findByRole('button', { name: 'Confirm' }).click()
+    })
+
+    cy.findByText('Submitted to AH for Input').should('exist')
+
+    cy.findByText(/successfully updated the status/g).should('exist')
+  })
 })
