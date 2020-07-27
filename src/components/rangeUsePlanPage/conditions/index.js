63bd82b3fbeb224f79d3337d97e578448c8a6256
Oamar Kanji <oamar.kanji@twostoryrobot.com>
2020-07-27 11:35:54 -0700

    hide range officer proposed conditions if user is myra client

--- src/components/rangeUsePlanPage/conditions/index.js
+++ src/components/rangeUsePlanPage/conditions/index.js
@@ -5,8 +5,10 @@ import { CONDITIONS } from '../../../constants/fields'
 import * as strings from '../../../constants/strings'
 import PermissionsField from '../../common/PermissionsField'
 import { TextArea } from 'formik-semantic-ui'
+import { useUser } from '../../../providers/UserProvider'
 
 const Conditions = ({ plan }) => {
+  const user = useUser()
   const { conditions = '', proposedConditions = '' } = plan || {}
 
   return (
@@ -39,31 +41,33 @@ const Conditions = ({ plan }) => {
             fast
           />
         </div>
-        <div className="rup__cell-6" style={{ marginBottom: 40 }}>
-          <div className="rup__info-title" style={{ width: 500 }}>
-            <div className="rup__popup-header">
-              Range Officer Recommendations
-              <InfoTip
-                header={strings.CONDITIONS}
-                content={strings.CONDITIONS_TIP_RANGE_OFFICER}
-                size="tiny"
-              />
+        {!user.roles.includes('myra_client') && (
+          <div className="rup__cell-6" style={{ marginBottom: 40 }}>
+            <div className="rup__info-title" style={{ width: 500 }}>
+              <div className="rup__popup-header">
+                Range Officer Recommendations
+                <InfoTip
+                  header={strings.CONDITIONS}
+                  content={strings.CONDITIONS_TIP_RANGE_OFFICER}
+                  size="tiny"
+                />
+              </div>
             </div>
+            <PermissionsField
+              permission={CONDITIONS.RANGE_OFFICER_CONDITIONS}
+              name="proposedConditions"
+              component={TextArea}
+              displayValue={
+                proposedConditions !== null ? '\n' + proposedConditions : ''
+              }
+              label={
+                'Consistent with FRPA 112 the following conditions are imposed and form part of the RUP approval:'
+              }
+              fieldProps={{ required: false }}
+              fast
+            />
           </div>
-          <PermissionsField
-            permission={CONDITIONS.RANGE_OFFICER_CONDITIONS}
-            name="proposedConditions"
-            component={TextArea}
-            displayValue={
-              proposedConditions !== null ? '\n' + proposedConditions : ''
-            }
-            label={
-              'Consistent with FRPA 112 the following conditions are imposed and form part of the RUP approval:'
-            }
-            fieldProps={{ required: false }}
-            fast
-          />
-        </div>
+        )}
       </div>
     </div>
   )
