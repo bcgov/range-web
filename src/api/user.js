853e179b903fcbd48a830f82f8cb71afaacd6eee
Caleb Sharp <caleb.sharp@twostoryrobot.com>
2020-07-29 14:28:01 -0700

    Use client number instead of id in link page

--- src/api/user.js
+++ src/api/user.js
@@ -1,46 +1,17 @@
 import * as API from '../constants/api'
 import { getAuthHeaderConfig, axios } from '../utils'
 
-export const createClientLink = async (userId, clientId) => {
+export const createClientLink = async (userId, clientNumber) => {
   return axios.post(
     API.CREATE_USER_CLIENT_LINK(userId),
-    { clientId },
+    { clientNumber },
     getAuthHeaderConfig()
   )
 }
 
-export const createAllClientLinks = async (userId, clientNumber) => {
-  const res = await axios.get(`v1/client`, getAuthHeaderConfig())
-  const { data: user } = await axios.get(
-    `${API.GET_USERS}/${userId}`,
-    getAuthHeaderConfig()
-  )
-
-  const clientsToLink = res.data.filter(
-    client =>
-      client.clientNumber === clientNumber &&
-      !user.clients.find(c => c.id === client.id)
-  )
-  if (clientsToLink.length === 0) {
-    throw new Error(
-      'All locations for this client number are already linked to this user'
-    )
-  }
-
-  for (const client of clientsToLink) {
-    await axios.post(
-      API.CREATE_USER_CLIENT_LINK(userId),
-      { clientId: client.id },
-      getAuthHeaderConfig()
-    )
-  }
-
-  return clientsToLink
-}
-
-export const deleteClientLink = async (userId, clientId) => {
+export const deleteClientLink = async (userId, clientNumber) => {
   return axios.delete(
-    API.DELETE_USER_CLIENT_LINK(userId, clientId),
+    API.DELETE_USER_CLIENT_LINK(userId, clientNumber),
     getAuthHeaderConfig()
   )
 }
